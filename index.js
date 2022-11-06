d7cf42ee6f0afcbb1630e7ff45d0b7ecf2c76790
yigithankardas <y.kardas@etu.edu.tr>
2022-11-07 02:54:54 +0300

    module conventions

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-const { Sequelize, DataTypes } = require('sequelize');
 const bp = require('body-parser');
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
-const sequelize = new Sequelize(
-  'postgres://postgres:2002@localhost:5432/project',
-  { define: { freezeTableName: true } }
-);
-
-try {
-  sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-
-const Auth = sequelize.define(
-  'auth',
-  {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    type: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
-
+const db = require('./src/db');
 const app = express();
 
 app.use(bp.json());
@@ -51,10 +13,9 @@ app.get('/api', (req, res) => {
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  sequelize
-    .query(
-      `select * from auth where username='${username}' and password='${password}'`
-    )
+  db.query(
+    `select * from auth where username='${username}' and password='${password}'`
+  )
     .then((data) => {
       const [result] = data[0];
       if (result !== undefined) {
---
+++ src/models/Auth.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+
+const Auth = db.define(
+  'auth',
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+modeule.exports = Auth;
