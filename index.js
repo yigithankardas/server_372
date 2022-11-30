9ff8ac0383f80aee50d5068733657b96dd9ce68a
OguzKaanOselmis <oguzkaanoselmis.ok@gmail.com>
2022-11-30 17:12:50 +0300

    getMappings added

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-const bp = require('body-parser');
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const db = require('./src/db');
+const bp = require("body-parser");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const db = require("./src/db");
 const app = express();
 
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
-  res.json('Hello World!');
+app.get("/api", (req, res) => {
+  res.json("Hello World!");
 });
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+app.get("/ilaclarim", (req, res) => {
+  const { TCNo } = req.body;
   db.query(
-    `select * from auth where username='${username}' and password='${password}'`
-  )
+    `select * from KULLANICI as U, KULLANIR as K, ILAC as I where U.TCNo='${TCNo}' and K.TCNo = U.TCNo and K.IlacId = I.IlacId`
+  ).then((data) => {
+    res.json(data[0]);
+  });
+});
+
+app.get("/asilarim", (req, res) => {
+  const { TCNo } = req.body;
+  db.query(
+    `select * from KULLANICI as K, ASI as S, YAPTIRIR as Y where K.TCNo = '${TCNo}' and K.TCNo = Y.TCNo and Y.AsiId = S.AsiId`
+  ).then((data) => {
+    res.json(data[0]);
+  });
+});
+
+// app.get("/randevularim", (req, res) => {
+//   const { TCNo } = req.body;
+//   db.query(``).then((data) => {
+//     res.json(data[0]);
+//   });
+// });
+
+app.post("/login", (req, res) => {
+  const { TCNo, Sifre } = req.body;
+  db.query(`select * from Kullanici where TCNo='${TCNo}' and Sifre='${Sifre}'`)
     .then((data) => {
       const [result] = data[0];
       if (result !== undefined) {
-        const token = jwt.sign({ username, password }, 'secretkey');
+        const token = jwt.sign({ TCNo, Sifre }, "secretkey");
         res.json({ token: token, type: result.type });
+      } else {
+        res.status(404).json();
       }
     })
     .catch((err) => {
@@ -29,5 +54,5 @@ app.post('/login', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('this is develop branch');
+  console.log("this is develop branch");
 });
