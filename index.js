eb17f748f7aa83b65764c95da5c17561148afb05
yigithankardas <y.kardas@etu.edu.tr>
2022-11-30 17:00:27 +0300

    Updated table names

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.get('/api', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  db.query(
-    `select * from auth where username='${username}' and password='${password}'`
-  )
+  const { TCNo, Sifre } = req.body;
+  db.query(`select * from kullanici where TCNo='${TCNo}' and Sifre='${Sifre}'`)
     .then((data) => {
       const [result] = data[0];
       if (result !== undefined) {
-        const token = jwt.sign({ username, password }, 'secretkey');
+        const token = jwt.sign({ TCNo, Sifre }, 'secretkey');
         res.json({ token: token, type: result.type });
+      } else {
+        res.status(404).json();
       }
     })
     .catch((err) => {
