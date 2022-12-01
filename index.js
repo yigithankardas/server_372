d7900545ea47a2ae5e524b460b5f9f217ed83be1
OguzKaanOselmis <oguzkaanoselmis.ok@gmail.com>
2022-12-01 14:52:01 +0300

    Asi ekleme yapilma tarihi guncelleme gibi degisiklikler icin PUT eklendi

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get('/api', (req, res) => {
   res.json('Hello World!');
 });
 
+app.get('/api/ilaclar', (req, res) => {
+  db.query(
+    'Select * From ILAC',
+  ).then((data) => {
+    res.json(data[0]);
+  });
+});
+
+// body'de TCsi verilen kullanicinin ilaclarinin doner
 app.get('/ilaclarim', (req, res) => {
   const { TCNo } = req.body;
   db.query(
@@ -23,6 +32,7 @@ app.get('/ilaclarim', (req, res) => {
   });
 });
 
+// body'de TCsi verilen kullanicinin asilarini doner
 app.get('/asilarim', (req, res) => {
   const { TCNo } = req.body;
   db.query(
@@ -32,6 +42,7 @@ app.get('/asilarim', (req, res) => {
   });
 });
 
+// body'de TCsi verilen kullanicinin randevularini doner
 app.get('/randevularim', (req, res) => {
   const { TCNo } = req.body;
   db.query(
@@ -41,6 +52,7 @@ app.get('/randevularim', (req, res) => {
   });
 });
 
+// body'de TCsi verilen kullanicinin profil bilgilerini doner
 app.get('/profilim', (req, res) => {
   const { TCNo } = req.body;
   db.query(`Select * From KULLANICI as K Where K.TCNo = '${TCNo}'`).then(
@@ -50,17 +62,53 @@ app.get('/profilim', (req, res) => {
   );
 });
 
+// Eger body'de siklik verilmediyse,
+// TCsi verilen kullanicinin id'si verilen ilacin kullanim sayisini 1 arttirir
+// Eger body'de siklik verildiyse,
+// Kullaniciya yeni ilac ekler
 app.put('/ilaclarim', (req, res) => {
-  const { TCNo, IlacId } = req.body;
-  db.query(`Update KULLANIR Set KullanmaSayisi=KullanmaSayisi+1 From KULLANICI as U, Ilac As I Where U.TCNo = '${TCNo}' and U.TCNo = KULLANIR.TCNo and I.IlacId = '${IlacId}' and I.IlacId = KULLANIR.IlacId;
+  const { TCNo, IlacId, Siklik } = req.body;
+
+  if (Siklik === undefined) {
+    db.query(`Update KULLANIR Set KullanmaSayisi=KullanmaSayisi+1 From KULLANICI as U, Ilac As I Where U.TCNo = '${TCNo}' and U.TCNo = KULLANIR.TCNo and I.IlacId = '${IlacId}' and I.IlacId = KULLANIR.IlacId;
   `).then(
-    (data) => {
-      res.json(data[0]);
-    },
-  );
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  } else {
+    db.query(`Insert Into KULLANIR Values('${TCNo}','${IlacId}','${Siklik}',0);
+  `).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  }
 });
 
-
+// Eger bodyde yapilma tarihi verilmediyse
+// Body'de tc si verilen kullaniciya asi'yi ekler
+// Eger bodyde yapilma tarihi verildiyse
+// Body'de tc si verilen kullanicinin kullandigi asinin yapilma tarihini gunceller
+app.put('/asilarim', (req, res) => {
+  const { TCNo, AsiId, YapilmaTarihi } = req.body;
+  console.log(`YAPILMA TARIHI: ${YapilmaTarihi}`);
+  if (YapilmaTarihi === undefined) {
+    db.query(`INSERT INTO YAPTIRIR VALUES('${TCNo}','${AsiId}',NULL);
+  `).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  } else {
+    db.query(`Update Yaptirir Set YapilmaTarihi='${YapilmaTarihi}' From KULLANICI as U, ASI as A Where U.TCNo = '${TCNo}' and U.TCNo = Yaptirir.TCNo and A.AsiId = '${AsiId}' and A.AsiId = YAPTIRIR.AsiId;
+`).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  }
+});
 
 app.post('/login', (req, res) => {
   const { TCNo, Sifre } = req.body;
