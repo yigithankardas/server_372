5bd46144aa20acc8843818bd2e8fbc3201d86f1d
OguzKaanOselmis <oguzkaanoselmis.ok@gmail.com>
2022-12-01 16:09:50 +0300

    Profil Bilgileri Güncelleme için PUT mappingler ve gerekli olabilecek bazı özel GET MAPPINGLER yazıldı

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable camelcase */
+
 const bp = require('body-parser');
 const express = require('express');
 const jwt = require('jsonwebtoken');
@@ -10,13 +13,49 @@ const app = express();
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
-  res.json('Hello World!');
-});
-
+// tum ilaclari doner
 app.get('/api/ilaclar', (req, res) => {
   db.query(
     'Select * From ILAC',
+  ).then((data) => {
+    res.json(data[0]);
+  });
+});
+
+// Eger HastaneId verilmediyse
+// tum doktorlari doner
+// Eger HastaneId verildiyse
+// O hastanede calisan doktorlari doner
+app.get('/api/doktorlar', (req, res) => {
+  const { HastaneId } = req.body;
+  if (HastaneId === undefined) {
+    db.query(
+      'Select * From DOKTOR',
+    ).then((data) => {
+      res.json(data[0]);
+    });
+  } else {
+    db.query(
+      `Select * From DOKTOR where HastaneId = '${HastaneId}'`,
+    ).then((data) => {
+      res.json(data[0]);
+    });
+  }
+});
+
+// tum ilaclari doner
+app.get('/api/asilar', (req, res) => {
+  db.query(
+    'Select * From ASI',
+  ).then((data) => {
+    res.json(data[0]);
+  });
+});
+
+// tum hastaneleri doner
+app.get('/api/hastaneler', (req, res) => {
+  db.query(
+    'Select * From HASTANE',
   ).then((data) => {
     res.json(data[0]);
   });
@@ -63,14 +102,14 @@ app.get('/profilim', (req, res) => {
 });
 
 // Eger body'de siklik verilmediyse,
-// TCsi verilen kullanicinin id'si verilen ilacin kullanim sayisini 1 arttirir
+// Kullanicinin id'si verilen ilacin kullanim sayisini Body'de verilen deger ile degistirir
 // Eger body'de siklik verildiyse,
 // Kullaniciya yeni ilac ekler
 app.put('/ilaclarim', (req, res) => {
-  const { TCNo, IlacId, Siklik } = req.body;
+  const { TCNo, IlacId, KullanmaSayisi, Siklik } = req.body;
 
   if (Siklik === undefined) {
-    db.query(`Update KULLANIR Set KullanmaSayisi=KullanmaSayisi+1 From KULLANICI as U, Ilac As I Where U.TCNo = '${TCNo}' and U.TCNo = KULLANIR.TCNo and I.IlacId = '${IlacId}' and I.IlacId = KULLANIR.IlacId;
+    db.query(`Update KULLANIR Set KullanmaSayisi='${KullanmaSayisi}' From KULLANICI as U, Ilac As I Where U.TCNo = '${TCNo}' and U.TCNo = KULLANIR.TCNo and I.IlacId = '${IlacId}' and I.IlacId = KULLANIR.IlacId;
   `).then(
       (data) => {
         res.json(data[0]);
@@ -92,7 +131,7 @@ app.put('/ilaclarim', (req, res) => {
 // Body'de tc si verilen kullanicinin kullandigi asinin yapilma tarihini gunceller
 app.put('/asilarim', (req, res) => {
   const { TCNo, AsiId, YapilmaTarihi } = req.body;
-  console.log(`YAPILMA TARIHI: ${YapilmaTarihi}`);
+
   if (YapilmaTarihi === undefined) {
     db.query(`INSERT INTO YAPTIRIR VALUES('${TCNo}','${AsiId}',NULL);
   `).then(
@@ -109,6 +148,57 @@ app.put('/asilarim', (req, res) => {
     );
   }
 });
+
+// Eger bodyde RandevuIsmi yoksa
+// DoktorTC si ve Tarihi verilen randevunun Gittimi degeri bodydeki ile degisir
+// Eger bodyde RandevuIsmi varsa
+// Verilen TCler tarih ve Randevu ismi ile yeni Randevu olusturulur
+app.put('/randevularim', (req, res) => {
+  const { KullaniciTc, DoktorTc, Tarih, Gitti_mi, RandevuIsmi } = req.body;
+
+  if (RandevuIsmi === undefined) {
+    db.query(`
+    Update RANDEVU Set Gitti_mi='${Gitti_mi}' From KULLANICI as U, DOKTOR As D Where U.TCNo = '${KullaniciTc}' and U.TCNo = RANDEVU.KullaniciTc and D.TCNo = RANDEVU.DoktorTc and RANDEVU.Tarih='${Tarih}';
+  `).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  } else {
+    db.query(`
+    INSERT INTO RANDEVU VALUES('${KullaniciTc}','${DoktorTc}','${RandevuIsmi}',0,'${Tarih}');
+    `).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  }
+});
+
+// Eger Body'de sifre verilmediyse
+// Diger ozellikler bodydeki degerlerle guncellenir
+// Eger Body'de sifre verildiyse
+// Sifre Bodydeki deger ile guncellenir
+app.put('/profilim', (req, res) => {
+  const { TCNo, Ad, Soyad, DogumT, Adres, Cinsiyet, Boy, Kilo, Sifre } = req.body;
+  console.log(`SIFRE: ${Sifre}`);
+  if (Sifre === undefined) {
+    db.query(`Update KULLANICI Set Ad='${Ad}', Soyad='${Soyad}', DogumT='${DogumT}', Adres='${Adres}', Cinsiyet='${Cinsiyet}', Boy='${Boy}', Kilo='${Kilo}' Where TCNo='${TCNo}';`).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  } else {
+    db.query(`Update KULLANICI Set Sifre='${Sifre}' Where TCNo='${TCNo}';`).then(
+      (data) => {
+        res.json(data[0]);
+      },
+    );
+  }
+
+});
+
+
 
 app.post('/login', (req, res) => {
   const { TCNo, Sifre } = req.body;
